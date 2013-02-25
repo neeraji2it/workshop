4ab429bc88761c58ed1b9c642deb722a2d763e3b
Neeraj C <neeraj@dine-media.com>
2013-02-25 09:01:59 +0530

    Chenges to fix db specific queries

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,7 @@ class Admin::DashboardController < ApplicationController
   before_filter :authenticate_user!
 
   def show
-    @songs = Song.with_vote_counts.order("vote_counts DESC")
+    #@songs = Song.with_vote_counts.order("vote_counts DESC")
+    @songs = Song.all.sort{|a, b| a.vote_count <=> b.vote_count}
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -15,7 +15,9 @@ class Song < ActiveRecord::Base
   scope :with_vote_counts, :select => "*, ((select count(*) from votes v1 where v1.created_at >= IFNULL(songs.last_played_at, songs.created_at) AND v1.song_id = songs.id AND v1.voted_up=1) - 2 * (select count(*) from votes v2 where v2.created_at >= IFNULL(songs.last_played_at, songs.created_at) AND v2.song_id = songs.id AND v2.voted_up=0)) as vote_counts"
 
   def self.upcoming
-    Song.with_vote_counts.order("vote_counts DESC").delete_if{|s| s.vote_counts < 2}
+    #Song.with_vote_counts.order("vote_counts DESC").delete_if{|s| s.vote_counts < 2}
+    songs = Song.all
+    return songs.delete_if{|s| s.vote_count < 2}.sort{|a, b| a.vote_count <=> b.vote_count}
   end
 
   def to_s
